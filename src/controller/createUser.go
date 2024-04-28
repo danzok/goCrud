80340f6ebf73a35ffd6e4789392e3d44cebe0e94
danzok <loppesofc@gmail.com>
2024-04-28 10:55:01 -0300

    add controller model response and request

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,24 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/danzok/goCrud.git/src/configuration/rest_err"
+	"github.com/danzok/goCrud.git/src/controller/model/request"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
 
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("there are some incorrect filds, error=%s\n", err.Error()),
+		)
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -8,7 +8,7 @@ import (
 func InitRoutes(r *gin.RouterGroup) {
 	r.GET("/getUserById/:userId", controller.FindUserById)
 	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
-	r.POST("createUser/", controller.CreateUser)
+	r.POST("/createUser", controller.CreateUser)
 	r.PUT("/updateUser/:userId", controller.UpdateUser)
 	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
 }
